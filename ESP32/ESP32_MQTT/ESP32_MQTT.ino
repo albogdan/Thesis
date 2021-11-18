8673c33f3210b4f895583e2c7b9107c4af80e2e5
AB <stefanalexb@gmail.com>
2021-11-17 20:37:32 -0500

    ESP32: - Implemented initial version of captive portal to set up WiFi information        - Captive Portal also contains option to set up the server address and port for your MQTT broker        - BUG: if the server information for your MQTT broker is incorrect, then it will not connect and just restart, because it will fail due to a NULL pointer in the MQTT broker class. Temp fix implemented where if it fails connecting it just infinite loops for handleCaptivePortal, so you can update the address and port

--- ESP32/ESP32_MQTT/ESP32_MQTT.ino
+++ ESP32/ESP32_MQTT/ESP32_MQTT.ino
@@ -4,14 +4,26 @@
 #include "esp_sntp.h"
 #include "driver/timer.h"
 #include "driver/rtc_io.h"
+#include <AutoConnect.h>
+#include <WebServer.h>
+#include "mqtt_params.h"
+#include <HTTPClient.h>
+#include <ESPmDNS.h>
 
+#include <FS.h>
+#include <SPIFFS.h>
+/* NOTE: CRASHES WHEN IT CAN'T CONNECT TO AN MQTT SERVER BECAUSE OF NULL POINTER
+ *  SHOULD MAKE A WAY SO THAT IT SWITCHES OVER TO SD CARD IF THAT HAPPENS
+ */
+
+ 
 /* Timer related defines */
 #define TIMER_DIVIDER         (16)  //  Hardware timer clock divider
 #define TIMER_SCALE           (TIMER_BASE_CLK / TIMER_DIVIDER)  // convert counter value to seconds
 
-#define SLEEP_TIMER_SECONDS 8 // Amount of time to wait until trying to go into hibernation mode
+#define SLEEP_TIMER_SECONDS 60 // Amount of time to wait until trying to go into hibernation mode
 #define RTC_GPIO_WAKEUP_PIN GPIO_NUM_4
-#define ARDUINO_SIGNAL_READY_PIN GPIO_NUM_5
+#define ARDUINO_SIGNAL_READY_PIN GPIO_NUM_23
 
 
 // Define sensor types
@@ -19,21 +31,39 @@
 #define SENSOR_HUMIDITY 2
 #define SENSOR_RAINFALL 3
 
-
 /* ----- BEGIN WIFI SETTINGS ----- */
 String HOSTNAME = "ESP32_LORA";
-const char* ssid     = "ssid_here";
-const char* password = "password_here";
-const char *mqttServer = "mqtt_server_here";
+char mqttServer[40] = "";
+char mqttPort[6] = "";
+WiFiClient espClient;
+WebServer web_server;
+AutoConnect captivePortal(web_server);
+
+const char* AUX_SETTING_URI = "/mqtt_setting";
+const char* AUX_SAVE_URI    = "/mqtt_save";
+// Declare AutoConnectElements for the page /mqtt_setting
+ACStyle(style, "label+input,label+select{position:sticky;left:120px;width:230px!important;box-sizing:border-box;}");
+ACText(header, "<h2>MQTT Broker Settings</h2>", "text-align:center;color:#2f4f4f;padding:10px;");
+ACText(caption, "Setup the Address and Port of the MQTT Server", "font-family:serif;color:#4682b4;");
+ACInput(mqttserver, "", "Address", "^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\\-]*[a-zA-Z0-9])\\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\\-]*[A-Za-z0-9])$", "Server Address");
+ACInput(mqttport, "", "Port", "^[0-9]{4-6}$", "Server Port");
+ACElement(newline, "<hr>");
+ACSubmit(save, "Save", AUX_SAVE_URI);
+AutoConnectAux mqtt_settings(AUX_SETTING_URI, "MQTT Settings", true, {style, header, caption, mqttserver, mqttport, newline, save});
+
+// Declare AutoConnectElements for the page /mqtt_save
+ACText(caption2, "<h4>Parameters saved as:</h4>", "text-align:center;color:#2f4f4f;padding:10px;");
+ACText(parameters);
+AutoConnectAux mqtt_save(AUX_SAVE_URI, "MQTT Setting", false, {caption2,parameters});
+
 /* ----- END WIFI SETTINGS ----- */
 
-/* ----- BEGIN MQTT SETTINGS ----- */
-const int mqttPort = 1883;
-long lastReconnectAttempt = 0;
-/* ----- END MQTT SETTINGS ----- */
 
-WiFiClient espClient;
+/* ----- BEGIN MQTT SETTINGS ----- */
+long lastReconnectAttempt = 0;
 PubSubClient mqttClient(espClient);
+
+/* ----- END MQTT SETTINGS ----- */
 
 /* ----- BEGIN PACKET INFORMATION AND DEFINITIONS ----- */
 //              Message parts and sizes diagram
@@ -98,23 +128,30 @@ void setup()
     delay(10);
     Serial.println("[SETUP] Serial2 setup complete");
 
-    // We start by connecting to a WiFi network
-    Serial.println();
-    Serial.println();
-    Serial.print("[SETUP] Connecting to ");
-    Serial.println(ssid);
+    // Load the MQTT config from SPIFFS
+    load_mqtt_config();
 
-    WiFi.begin(ssid, password);
+    // Add the MQTT pages to the Captive Portal
+    captivePortal.join({mqtt_settings, mqtt_save});
 
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
+    captivePortal.on(AUX_SETTING_URI, loadParams);
+    captivePortal.on(AUX_SAVE_URI, saveParams);
+
+
+    // Restore configured MQTT broker settings
+    PageArgument  args;
+    AutoConnectAux& mqtt_setting = *captivePortal.aux(AUX_SETTING_URI);
+    loadParams(mqtt_setting, args);
+
+    // Now we connect to a WiFi network
+    Serial.println("[SETUP] Setting up the Captive WiFi Portal");
+    if (captivePortal.begin()) {
+      Serial.println("");
+      Serial.println("[INFO] WiFi connected");
+      Serial.print("[INFO] IP address: ");
+      Serial.println(WiFi.localIP());  
     }
-
-    Serial.println("");
-    Serial.println("[INFO] WiFi connected");
-    Serial.print("[INFO] IP address: ");
-    Serial.println(WiFi.localIP());
+    
 
     Serial.println("[SETUP] Setting up MQTT server...");
     setupMqtt(mqttServer, mqttPort);
@@ -149,7 +186,6 @@ void setup()
         }
     }
 
-    
     Serial.print("[INFO] The current time is: ");
     update_current_local_time();
     Serial.println(strftime_buf);
@@ -157,10 +193,84 @@ void setup()
     Serial.println("[INFO] Initializing sleep timer");
     initialize_sleep_timer(SLEEP_TIMER_SECONDS);
 
-    Serial.println("[SETUP] Boot sequence complete.");
     pinMode(ARDUINO_SIGNAL_READY_PIN, OUTPUT);
     digitalWrite(ARDUINO_SIGNAL_READY_PIN, HIGH);
+    Serial.println("[SETUP] Boot sequence complete.");
 }
+
+// Retreive the value of each element entered by '/mqtt_setting'.
+String saveParams(AutoConnectAux& aux, PageArgument& args) {
+  mqttserver.value.trim();
+  mqttport.value.trim();
+
+  Serial.println("saving config");
+  if (SPIFFS.begin()) {
+    Serial.println("mounted file system");
+    if (SPIFFS.exists("/config.json")) {
+      //file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open("/config.json", "w");
+      if (configFile) {
+        Serial.println("opened config file");
+        DynamicJsonDocument json(1024);
+        json["mqtt_server"] = mqttserver.value.c_str();
+        json["mqtt_port"] = mqttport.value.c_str();
+        
+        serializeJson(json, Serial);
+        serializeJson(json, configFile);
+        configFile.close();
+      }
+    }
+  } else {
+    Serial.println("failed to mount FS");
+  }  
+  // Echo back saved parameters to AutoConnectAux page.
+  String echo = "Server: " + mqttserver.value + "<br>";
+  echo += "Port: " + mqttport.value + "<br>";
+  parameters.value = echo;
+  return String("");
+}
+
+// Load parameters from persistent storage
+String loadParams(AutoConnectAux& aux, PageArgument& args) {
+  load_mqtt_config();
+  mqttserver.value = mqttServer;
+  mqttport.value = mqttPort;
+  return String("");
+}
+
+void load_mqtt_config() {
+  if (SPIFFS.begin()) {
+    Serial.println("mounted file system");
+    if (SPIFFS.exists("/config.json")) {
+      //file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile) {
+        Serial.println("opened config file");
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonDocument json(1024);
+        auto deserializeError = deserializeJson(json, buf.get());
+        serializeJson(json, Serial);
+        if ( ! deserializeError ) {
+          Serial.println("\nparsed json");
+          strcpy(mqttServer, json["mqtt_server"]);
+          strcpy(mqttPort, json["mqtt_port"]);
+        } else {
+          Serial.println("failed to load json config");
+        }
+        configFile.close();
+      }
+    }
+  } else {
+    Serial.println("failed to mount FS");
+  }  
+}
+
 
 void update_current_local_time(){
     time(&now);
@@ -169,20 +279,9 @@ void update_current_local_time(){
 }
 
 void loop() {
-  // loop_count++;
-  // if (loop_count >= 5000) {
-  //     Serial.println("[INFO] Entering hibernate mode");
-  //     loop_count = 0;
-  //     hibernate_mode();
-  // }
-
-  // Check to make sure the MQTT connection is live
-  check_mqtt_connection();
 
   // Check if we received a complete string through Serial2
   if (byte_array_complete) {
-    // Publish the string to MQTT
-//    publish_arduino_data(input_byte_array);
     parse_input_string(input_byte_array);
 
     // Clear the byte array: reset the index pointer
@@ -194,8 +293,7 @@ void loop() {
   while (Serial2.available()) {
     // Get the new byte:
     byte inByte = (byte)Serial2.read();
-    //Serial.print("RECV: 0x");
-    //Serial.println(inByte, HEX);
+
     // Add it to the inputString:
     input_byte_array[input_byte_array_index] = inByte;
     input_byte_array_index += 1;
@@ -206,6 +304,9 @@ void loop() {
       byte_array_complete = true;
     }
   }
+  captivePortal.handleClient();
+  mqttClient.loop();
+  
 }
 
 void parse_input_string(byte *input_byte_array) {
@@ -320,15 +421,20 @@ void publish_packet_to_mqtt(Packet payload) {
     Serial.print("Publishing to MQTT: ");
     Serial.println(message);
     Serial.println();
-    mqttClient.publish(topic_char_array, message_char_array);
+    
+    if (mqttClient.connected()) {
+      mqttClient.publish(topic_char_array, message_char_array);
+    }  else {
+      reconnect_mqtt_connection();
+    }
 
     yield();
 }
 
 
-void setupMqtt(const char *mqttServer, const int mqttPort)
+void setupMqtt(char *mqttServer, char *mqttPort)
 {
-    mqttClient.setServer(mqttServer, mqttPort);
+    mqttClient.setServer(mqttServer, atoi(mqttPort));
     mqttClient.setCallback(callbackMqtt);
     mqttClient.setBufferSize(1024);   // MAKE SURE TO CHECK THAT THIS IS BIG ENOUGH
 }
@@ -339,7 +445,10 @@ boolean connectToMqtt()
     HOSTNAME.toCharArray(hostname_char, HOSTNAME.length() + 1);
     while (!mqttClient.connected())
     {
-        Serial.println("[SETUP] Connecting to MQTT...");
+        Serial.print("[SETUP] Connecting to MQTT at ");
+        Serial.print(mqttServer);
+        Serial.print(":");
+        Serial.println(mqttPort);
         if (mqttClient.connect(hostname_char))
         { //, mqttUser, mqttPassword )) {
             Serial.print("[INFO] MAC: ");
@@ -351,17 +460,23 @@ boolean connectToMqtt()
         else
         {
             Serial.print("[SETUP] Connection failed with state ");
-            Serial.print(mqttClient.state());
+            Serial.println(mqttClient.state());
             delay(200);
+
+            // Temp fix to allow for you to change the MQTT information if its not connected
+            while(1) {
+              captivePortal.handleClient();
+            }
             return false;
         }
+        
     }
 }
 
 
-void check_mqtt_connection()
+void reconnect_mqtt_connection()
 {
-    if (!mqttClient.connected())
+    if (WiFi.status() == WL_CONNECTED)
     {
         long now = millis();
         if (now - lastReconnectAttempt > 10000)
@@ -441,8 +556,11 @@ void return_ping()
 
     Serial.print("Replying with: ");
     Serial.println(message);
-    mqttClient.publish(topic_output, message_output);
-
+    if (mqttClient.connected()) {
+      mqttClient.publish(topic_output, message_output);
+    }  else {
+      reconnect_mqtt_connection();
+    }
     yield();
 }
 
@@ -466,8 +584,11 @@ void publish_arduino_data(String arduino_data) {
 
     Serial.print("Publishing to MQTT: ");
     Serial.println(message);
-    mqttClient.publish(topic_char_array, message_char_array);
-
+    if (mqttClient.connected()) {
+      mqttClient.publish(topic_char_array, message_char_array);
+    }  else {
+      reconnect_mqtt_connection();
+    }
     yield();
 }
 
