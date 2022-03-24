92c4c237f287ef0f9657200198b0f6e82f060471
AB <stefanalexb@gmail.com>
2022-03-24 11:37:48 -0400

    Added graphs for message counts

---
+++ Graphs/ESP32_MessageCounts/ESP32_Message_Counts_Graph.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+plt.style.use("seaborn-whitegrid")
+import numpy as np
+import pandas as pd
+
+
+#Num Messages,Length of Duty Cycle (s),Charge (C),Charge (mAh)
+FILE_NAME = "message_counts.csv"
+
+# Read the dataframe from the CSV
+df = pd.read_csv(FILE_NAME, sep=",", header=0)
+
+# Divide the uA by 1000 to get mA
+#df["Current(uA)"] = df["Current(uA)"].div(1000)
+
+
+print(df)
+# Mask around the times you wish to show in the plot
+
+#mask = (df["Timestamp(ms)"] > START_TIME) & (df["Timestamp(ms)"] < END_TIME)
+#sub_df = df.loc[mask]
+#sub_df["Timestamp(ms)"] = sub_df["Timestamp(ms)"].sub(START_TIME).div(1000)
+
+# Plot it
+fig, ax = plt.subplots(figsize=(12, 4))
+plt.ylim([0,30])
+df[["Num Messages", "Length of Duty Cycle (s)"]].plot(x="Num Messages", y="Length of Duty Cycle (s)", ax=ax, legend=None, marker='o')
+
+# Axis labels
+ax.set_xlabel("Number of Messages")
+ax.set_ylabel("Length of Duty Cycle (s)")
+#ax.set_title("ESP32 Profiling")
+
+fig.savefig("ESP32_NumMessages_LengthDutyCycle.png", bbox_inches='tight')
+plt.show()
+plt.clf()
+
+
+fig, ax = plt.subplots(figsize=(12, 4))
+plt.ylim([0,1.5])
+df[["Num Messages", "Charge (mAh)"]].plot(x="Num Messages", y="Charge (mAh)", ax=ax, legend=None, marker='o')
+
+# Axis labels
+ax.set_xlabel("Number of Messages")
+ax.set_ylabel("Charge (mAh)")
+#ax.set_title("ESP32 Profiling")
+
+fig.savefig("ESP32_NumMessages_ChargemAh.png", bbox_inches='tight')
+plt.show()
+plt.clf()
+
+
+
+
