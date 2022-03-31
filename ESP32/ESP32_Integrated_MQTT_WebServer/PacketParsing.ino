076550119d5a2f5e86244231ed0d269813d76f5d
AB <stefanalexb@gmail.com>
2022-03-30 21:00:11 -0400

    Fixed packet parsing decimal places

--- ESP32/ESP32_Integrated_MQTT_WebServer/PacketParsing.ino
+++ ESP32/ESP32_Integrated_MQTT_WebServer/PacketParsing.ino
@@ -1,3 +1,7 @@
+typedef union{
+  float f;
+  byte b[4];
+}doubleConverter;
 
 void parse_input_string(byte *input_byte_array) {
     Packet packet;
@@ -9,6 +13,16 @@ void parse_input_string(byte *input_byte_array) {
     // input_byte_array[5] = 0
     // input_byte_array[6] = 0
     // input_byte_array[7] = data start
+    /*Serial.print("0: 0x");
+    Serial.println(input_byte_array[0], HEX);
+    Serial.print("1: 0x");
+    Serial.println(input_byte_array[1], HEX);
+    Serial.print("2: 0x");
+    Serial.println(input_byte_array[2], HEX);
+    Serial.print("3: 0x");
+    Serial.println(input_byte_array[3], HEX);
+    Serial.print("4: 0x");
+    Serial.println(input_byte_array[4], HEX);
     Serial.print("5: 0x");
     Serial.println(input_byte_array[5], HEX);
     Serial.print("6: 0x");
@@ -24,21 +38,23 @@ void parse_input_string(byte *input_byte_array) {
     Serial.print("11: 0x");
     Serial.println(input_byte_array[11], HEX);
     Serial.print("12: 0x");
-    Serial.println(input_byte_array[12], HEX);
+    Serial.println(input_byte_array[12], HEX);*/
     if (packet.src[0] == 0 && packet.src[1] == 0) {
       Serial.println("Initial packets, ignoring");
     } else {
-      Serial.print("Packet src: 0x");
+      /*Serial.print("Packet src: 0x");
       Serial.print(packet.src[0], HEX);
       Serial.println(packet.src[1], HEX);
       Serial.print("Packet length: ");
-      Serial.println(packet.message_length);
-      byte float_data[4] = {0x0, 0x0, input_byte_array[7], input_byte_array[8]};
+      Serial.println(packet.message_length);*/
+      byte float_data[4] = {input_byte_array[5], input_byte_array[6], input_byte_array[7], input_byte_array[8]};
+
       packet.data_value = *( (float*) float_data );
-      Serial.print("Message: ");
+      /*Serial.print("Message: ");
       Serial.println(packet.data_value);
-      Serial.println(packet.data_value, HEX);
+      Serial.println(packet.data_value, HEX);*/
       publish_packet_to_mqtt(packet);
+      //print_packet(packet);
     }
 }
 
@@ -82,6 +98,7 @@ void publish_packet_to_mqtt(Packet payload) {
     Serial.println(message);
     Serial.println();
     mqttClient.publish(topic_char_array, message_char_array);
-
+    //digitalWrite(ARDUINO_SIGNAL_READY_PIN, LOW);
+    
     yield();
 }
